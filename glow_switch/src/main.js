69ac100c7a32afa5ce7a6630a3708daabcb4822d
Terrence Cole <terrence.d.cole@gmail.com>
2020-03-07 18:11:05 -0800

    More color, more stability.

--- glow_switch/src/main.js
+++ glow_switch/src/main.js
@@ -18,6 +18,10 @@ let espruino = require('../src/hardware/espruino_wifi.js');
 let colorspace = require('../src/util/colorspace.js');
 let WebThing = require('../src/webthing.js');
 
+const REPEAT_TIMEOUT = 15;
+const DEBOUNCE_TIMEOUT = 30;
+const RESTORE_TIMEOUT = 1000;
+
 SPI1.setup({mosi:A7, sck:A5});
 function set_color(r, g, b) {
   const HEADER = [0x00, 0x00, 0x00, 0x00];
@@ -26,62 +30,6 @@ function set_color(r, g, b) {
   let buffer = HEADER.concat(pix, pix, pix, pix, FOOTER);
   SPI1.write(buffer);
 }
-
-/*
-class State {
-  constructor() {
-    this.properties = {
-      most_recent_button_press: {
-        value: 0,
-        title: 'MostRecentButtonPress',
-        type: 'integer',
-        readOnly: true,
-        description: 'The most recently pressed button'
-      },
-      color: {
-        value: 'none',
-        title: 'Color',
-        type: 'string',
-        readOnly: false,
-        description: 'The color to set the LEDs'
-      },
-      effect: {
-        value: 'solid',
-        title: 'EffectStyle',
-        type: 'string',
-        enum: ['solid'],
-        readOnly: false,
-        description: 'The effect style with which to drive the LEDs'
-      }
-    };
-    this.property_watchers = {};
-  }
-
-  get_properties() {
-    return this.properties.keys();
-  }
-
-  get_property(name) {
-    return this.properties[name].value;
-  }
-
-  set_property(name, value) {
-    this.properties[name].value = value;
-    for (let key in this.property_watchers) {
-      const watch_func = this.property_watchers[key];
-      watch_func(name, value);
-    }
-  }
-
-  watch_properties(watch_name, watch_func) {
-    this.property_watchers[watch_name] = watch_func;
-  }
-
-  unwatch_properties(watch_name) {
-    delete this.property_watchers[watch_name];
-  }
-}
-*/
 
 function State() {
   this._properties = {
@@ -103,9 +51,16 @@ function State() {
       value: 'solid',
       title: 'EffectStyle',
       type: 'string',
-      enum: ['solid'],
+      enum: ['solid', 'wave'],
       readOnly: false,
       description: 'The effect style with which to drive the LEDs'
+    },
+    temperature: {
+      value: E.getTemperature(),
+      title: 'Temperature',
+      type: 'number',
+      readOnly: true,
+      description: 'The value of the onboard temperature sensor'
     }
   };
   this._property_watchers = {};
@@ -151,28 +106,61 @@ State.prototype.unwatch_properties = function(watch_name) {
   delete this._property_watchers[watch_name];
 };
 
-function onButtonPress(pin, state, thing) {
-  const PIN_MAP = {
-    A0: 0,
-    A1: 1,
-    B9: 2,
-    B8: 3
-  };
+const PIN_MAP = {
+  A0: 0,
+  A1: 1,
+  B9: 2,
+  B8: 3
+};
 
-  let next_state = PIN_MAP[pin];
-  if (next_state == state.get_property('most_recent_button_press')) {
+function onStartButtonPress(initial_pin_value, pin, state, thing) {
+  let initial_time = getTime();
+  if (initial_pin_value != false) {
+    // console.log('' + initial_time + ': reject  ' + PIN_MAP[pin]);
     return;
   }
+
+  // console.log('' + initial_time + ': waiting ' + PIN_MAP[pin]);
+  setTimeout(function() {
+    let sample_time = getTime();
+    let final_pin_value = !!digitalRead(pin);
+    if (initial_pin_value == final_pin_value) {
+      // console.log('' + sample_time + ': press   ' + PIN_MAP[pin] + ' : ' + initial_time + ' -> ' + sample_time + ' => ' + final_pin_value);
+      onFinishButtonPress(pin, state, thing);
+    } else {
+      // console.log('' + sample_time + ': filter  ' + PIN_MAP[pin]);
+    }
+  }, DEBOUNCE_TIMEOUT);
+}
+
+let prior_state = -1;
+function onFinishButtonPress(pin, state, thing) {
+
+  let next_state = PIN_MAP[pin];
+  if (next_state == prior_state) {
+    return;
+  }
+  prior_state = next_state;
   state.set_property('most_recent_button_press', next_state);
+
+  setTimeout(function() {
+    prior_state = -1;
+  }, RESTORE_TIMEOUT);
 
   // Emit events too?
   //thing.send_event('button_press', next_state);
 }
 
+function make_watch_func(pin, state, thing) {
+  return function(context) {
+    onStartButtonPress(context.state, pin, state, thing);
+  };
+}
+
+const HEADER = [0x00, 0x00, 0x00, 0x00];
+const FOOTER = [0xFF, 0xFF, 0xFF, 0xFF];
 let prior_color = '';
-function set_solid_color_interval(state) {
-  const HEADER = [0x00, 0x00, 0x00, 0x00];
-  const FOOTER = [0xFF, 0xFF, 0xFF, 0xFF];
+function set_solid_color_interval(state, dim) {
   let color = state.get_property('color');
   if (prior_color === color) {
     return;
@@ -180,38 +168,149 @@ function set_solid_color_interval(state) {
   prior_color = color;
 
   let [r, g, b] = colorspace.parse_html(color);
-  let pix = [0xff, b, g, r];
+  let pix0 = [0xff, b, g, r];
+  let pix1 = pix0;
+  if (dim) {
+    pix1 = [0xff, 0, 0, 0];
+  }
 
-  let buffer = HEADER.concat(pix, pix, pix, pix, FOOTER);
+  let buffer = HEADER.concat(pix0, pix1, pix1, pix1, FOOTER);
   SPI1.write(buffer);
+}
+
+function clamp(v) {
+  return Math.min(255, Math.max(0, v));
+}
+
+let time_offset = 0;
+function set_wave_color_interval(state) {
+  let color = state.get_property('color');
+  let [r, g, b] = colorspace.parse_html(color);
+
+  function mkpix(period, r, g, b) {
+    let w = time_offset * 3.14 / 180 * period;
+    return [
+      0xff,
+      clamp(b + (b / 2) * Math.sin(w)),
+      clamp(g + (g / 2) * Math.sin(w)),
+      clamp(r + (r / 2) * Math.sin(w))
+    ];
+  }
+
+  let buffer = HEADER.concat(
+    mkpix(1 / 3, r, g, b),
+    mkpix(1 / 9, r, g, b),
+    mkpix(1 / 7, r, g, b),
+    mkpix(1 / 11, r, g, b),
+    FOOTER
+  );
+  SPI1.write(buffer);
+
+  time_offset += 16;
+  time_offset %= 160000;
+}
+
+function set_swirl_color_interval(state) {
+  const interval = 6000;
+  let h = (time_offset % interval) / interval;
+  let [r, g, b] = E.HSBtoRGB(h, 1, 0.5, true);
+
+  function mkpix(period, r, g, b) {
+    let w = time_offset * 3.14 / 180 * period;
+    return [
+      0xff,
+      clamp(b + (b / 2) * Math.sin(w)),
+      clamp(g + (g / 2) * Math.sin(w)),
+      clamp(r + (r / 2) * Math.sin(w))
+    ];
+  }
+
+  let buffer = HEADER.concat(
+    mkpix(1 / 3, r, g, b),
+    mkpix(1 / 9, r, g, b),
+    mkpix(1 / 7, r, g, b),
+    mkpix(1 / 11, r, g, b),
+    FOOTER
+  );
+  SPI1.write(buffer);
+
+  time_offset += 16;
+  time_offset %= 16000000;
+}
+
+function set_flame_color_interval(state) {
+  let color = state.get_property('color');
+  let [r, g, b] = colorspace.parse_html(color);
+
+  function mkpix(period, amplitude, r, g, b) {
+    let w = time_offset * 3.14 / 180;
+    let wind_effect = (1 + Math.sin(w / 7)) / 2 * (1 + Math.sin(w / 5)) / 2;
+    let flicker = wind_effect * Math.random();
+    return [
+      0xff,
+      clamp(b + (b * amplitude + flicker * 5) * Math.sin(w * period + flicker)),
+      clamp(g + (g * amplitude + flicker * 5) * Math.sin(w * period + flicker)),
+      clamp(r + (r * amplitude + flicker * 5) * Math.sin(w * period + flicker))
+    ];
+  }
+
+  let buffer = HEADER.concat(
+    mkpix(1 / 2, 1 / 8, 128, 64, 0),
+    mkpix(1 / 3, 1 / 6, 192, 32, 0),
+    mkpix(1 / 7, 1 / 2, 96, 96, 32),
+    mkpix(1 / 5, 1 / 4, 128, 64, 0),
+    FOOTER
+  );
+  SPI1.write(buffer);
+
+  time_offset += 16;
+  time_offset %= 160000;
 }
 
 let thing = null;
 function onConnected() {
   let state = state = new State();
   thing = new WebThing(config.thing, state);
+  // state.set_property('color', 'rgb(128, 64, 0)');
+  // state.set_property('effect', 'swirl');
 
-  const opts = { repeat: true };
+  // Button: listen for presses
   pinMode(A0, 'input_pullup');
-  setWatch(function(_state, _time, _lastTime) { onButtonPress(A0, state, thing); }, A0, opts);
   pinMode(A1, 'input_pullup');
-  setWatch(function(_state, _time, _lastTime) { onButtonPress(A1, state, thing); }, A1, opts);
   pinMode(B9, 'input_pullup');
-  setWatch(function(_state, _time, _lastTime) { onButtonPress(B9, state, thing); }, B9, opts);
   pinMode(B8, 'input_pullup');
-  setWatch(function(_state, _time, _lastTime) { onButtonPress(B8, state, thing); }, B8, opts);
+  const opts = { repeat: true, debounce: REPEAT_TIMEOUT };
+  setWatch(make_watch_func(A0, state, thing), A0, opts);
+  setWatch(make_watch_func(A1, state, thing), A1, opts);
+  setWatch(make_watch_func(B9, state, thing), B9, opts);
+  setWatch(make_watch_func(B8, state, thing), B8, opts);
 
+  // Color: refresh driver
   setInterval(function() {
     let effect = state.get_property('effect');
-    if (effect === 'solid') {
-      set_solid_color_interval(state);
+    if (effect === 'wave') {
+      set_wave_color_interval(state);
+    } else if (effect === 'swirl') {
+      set_swirl_color_interval(state);
+    } else if (effect === 'flame') {
+      set_flame_color_interval(state);
+    } else if (effect === 'dim') {
+      set_solid_color_interval(state, true);
+    } else {
+      set_solid_color_interval(state, false);
     }
   }, 16);
+
+  // Temperature: update once a minute
+  setInterval(function() {
+    state.set_property('temperature', E.getTemperature());
+  }, 60000);
 }
 
 function main() {
   set_color(255, 0, 0);
   espruino.connect_to_wifi(config.wifi, onConnected);
+  // onConnected();
 }
 
 E.on('init', main);
