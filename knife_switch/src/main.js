5e3b88c96a6c4ffc36f763b22aafc42414ee9622
Terrence Cole <terrence.d.cole@gmail.com>
2020-03-14 14:17:07 -0700

    Update ks to latest.

--- knife_switch/src/main.js
+++ knife_switch/src/main.js
@@ -1,7 +1,22 @@
+// This file is part of Redstone.
+//
+// Redstone is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Redstone is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Redstone.  If not, see <http://www.gnu.org/licenses/>.
 'use strict';
-let wifi = require('Wifi');
 import config from './config.js';
-import webthing from './thing.js';
+import espruino from '../node_modules/redstone/src/hardware/espruino_wifi.js';
+import WebThing from '../node_modules/redstone/src/webthing.js';
+import WebThingState from '../node_modules/redstone/src/state.js';
 
 // On
 pinMode(B10, 'input_pullup');
@@ -14,45 +29,6 @@ pinMode(A0, 'input_pullup');
 pinMode(A1, 'input_pullup');
 pinMode(B0, 'input_pullup');
 pinMode(B9, 'input_pullup');
-
-let error_flasher = null;
-let wifi_initialized = false;
-function connect_to_wifi(complete) {
-  if (wifi_initialized) {
-    return;
-  }
-  wifi_initialized = true;
-  LED1.write(true);
-  wifi.connect(config.wifi.ssid, { 'password': config.wifi.password }, function(err) {
-    if (err) {
-      console.log('Connection error: ' + err);
-      connection_error();
-      return;
-    }
-    console.log('Connected!');
-    wifi.getIP(function(err, data) {
-      if (err) {
-        console.log('ERROR: no IP: ' + err);
-        return;
-      }
-      console.log('IP:  ' + data.ip);
-      console.log('MAC: ' + data.mac);
-    });
-    if (error_flasher) {
-      clearInterval(error_flasher);
-    }
-    LED1.write(false);
-    complete();
-  });
-}
-
-function connection_error() {
-  let on = false;
-  error_flasher = setInterval(function() {
-    on = !on;
-    LED1.write(on);
-  }, 500);
-}
 
 function read_switch_state() {
   let status = digitalRead([B10, B13, B14, B15, A0, B0, A1, B9]);
@@ -72,63 +48,47 @@ function read_switch_state() {
   return 'down';
 }
 
-class State {
-  constructor() {
-    this.properties = {
-      position: {
-        value: read_switch_state(),
-        title: 'Position',
-        type: 'string',
-        enum: ['up', 'open', 'down'],
-        readOnly: true,
-        description: 'The switch position'
-      }
-    };
-    this.property_watchers = {};
-  }
-
-  get_property(name) {
-    return this.properties[name].value;
-  }
-
-  set_property(name, value) {
-    this.properties[name].value = value;
-    for (let key in this.property_watchers) {
-      const watch_func = this.property_watchers[key];
-      watch_func(name, value);
-    }
-  }
-
-  watch_properties(watch_name, watch_func) {
-    this.property_watchers[watch_name] = watch_func;
-  }
-
-  unwatch_properties(watch_name) {
-    delete this.property_watchers[watch_name];
-  }
-}
-let state = new State();
-console.log('initial switch state: ' + state.get_property('position'));
-
-function onStateChange(_state, _time, _lastTime) {
+function onStateChange(state) {
   let next_state = read_switch_state();
   if (next_state == state.get_property('position')) {
     return;
   }
   state.set_property('position', next_state);
-
-  console.log('new state: ' + next_state);
 }
 
-function onInit() {
-  connect_to_wifi(function() {
-    let opts = { repeat: true };
-    let watchablePins = [A0, A1, B9, B10, B13, B14, B15];
-    for (const pin of watchablePins) {
-      setWatch(onStateChange, pin, opts);
-    }
-
-    let thing = webthing.start(config.thing, state);
+function onConnected() {
+  let state = new WebThingState();
+  state.create_property({
+    name: 'position',
+    type: 'string',
+    enumerants: ['up', 'open', 'down'],
+    initial_value: read_switch_state(),
+    title: 'Position',
+    description: 'The current position of the knife switch',
+    read_only: true
   });
+  state.create_property({
+    name: 'temperature',
+    initial_value: E.getTemperature(),
+    title: 'Temperature',
+    type: 'number',
+    description: 'The value of the onboard temperature sensor'
+  });
+  let thing = new WebThing(config.thing, state);
+
+  let opts = { repeat: true };
+  let watchablePins = [A0, A1, B9, B10, B13, B14, B15];
+  for (const pin of watchablePins) {
+    setWatch(function(_context, _time, _location) { onStateChange(state); }, pin, opts);
+  }
+
+  // Temperature: update once a minute
+  setInterval(function() {
+    state.set_property('temperature', E.getTemperature());
+  }, 60000);
 }
-onInit();
+
+function main() {
+  espruino.connect_to_wifi(config.wifi, onConnected);
+}
+E.on('init', main);
